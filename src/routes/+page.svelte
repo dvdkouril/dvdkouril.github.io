c0486c3be5a85b65688d24801810cd876af030fa
David Kouril <david.kouril@gmail.com>
2024-04-05 11:19:59 -0400

    move from new landing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<svelte:head>
+	<title>Hello world!</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<h1>DAVID KOUŘIL.</h1>
+<h2><pre>David Kou\v{'{'}r{'}'}il</pre></h2>
+
+<div id="container">
+	<p>Hi, I'm currently a postdoc at <a href="http://hms.harvard.edu">Harvard Medical School</a> in Nils Gehlenborg's <a href="http://hidivelab.org">HIDIVE Lab</a>.</p>
+
+	<p>I work in data visualization research, specializing on biological data. At HIDIVE, I develop tools for visualization of 3D chromatin structure predictions as a complement for the typical chromosome conformation capture techniques used for determining spatial organization of genomes.</p>
+
+	<p>Currently, I'm making a Javascript <a href="https://observablehq.com/@david-kouril/chromospace-playground">library for 3D chromatin visualization</a>. We're also preparing a widget version intended for use in <a href="https://colab.research.google.com/drive/1E-jDzM1hs-TtTg_XJnEuDn6RU2VG0Lxe?usp=sharing">computational notebooks</a>.</p>
+
+	<p>I received a doctoral degree from <a href="https://www.tuwien.at/en/">TU Wien</a>. My thesis <em>Interactive Visualization of Dense and Multi-Scale Data for Science Outreach</em> was awarded the <a href="https://www.eg.org/wp/eurographics-awards-programme/eurovis-phd-award/">EuroVis PhD Award in 2022</a>.</p>
+
+	<p>I was born in <a href="https://maps.app.goo.gl/ctHG1FnMsnKei2FN7">Zlín, Czech Republic</a>, and lived in <a href="https://maps.app.goo.gl/e4W2jxMeZuTjPTFW9">Brno</a> for several years.</p>
+</div>
+
+
+<style>
+* {
+	font-family: "Dosis", sans-serif;
+}
+
+#container {
+	width: 400px;
+	text-align: justify;
+	line-height: 1.3;
+	}
+
+	h1 {
+	padding-bottom: 0;
+	margin-bottom: 0;
+	}
+
+	h2 {
+	color: #aaaaaa;
+	padding-top: 0;
+	margin-top: 0;
+	}
+	h2 > pre {
+	padding-top: 0;
+	margin-top: 0;
+	}
+
+	a {
+	color: white;
+	font-size: 90%;
+	background-color: red;
+	padding: 0px 5px 0px 5px;
+	margin-top: 5px;
+	text-decoration: none;
+	}
+</style>
---
+++ src/routes/hidive/+page.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<title>3D genomics @ HIDIVE</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,100&display=swap" rel="stylesheet">
+</svelte:head>
+
+<h1>3D genomics @ HIDIVE</h1>
+<p>
+My goal in Nil Gehlenborg's HIDIVE Lab is to improve visualization tools for 3D chromatin structural data. Many tools have been developed for genomics and investigation of chromatin's spatial structure is important. However, the tools currently used in day-to-day practice are mostly from proteomics.
+</p>
+
+<p>
+Projects:
+</p>
+<ul>
+	<li>chromospace: javascript library for visualization of 3D chromatin structures</li>
+	<li>chromospyce: anywidget-powered computational notebook version of chromospace</li>
+	<li>hyperwindows: visualization technique for navigating complex 3D structures via a hierarchical layout and interactions</li>
+</ul>
+
+<p>Prototypes:</p>
+<ul>
+	<li>hierarchical-selections prototype: hierarchical selections in a 3D chromatin model, visualization via a fixed tree-like layout. (code)</li>
+	<li>chromatin-n-out (aka hyperwindows) prototype: hierarchical selections in a 3D chromatin model, visualization via a fluid physics-based layout. (code)</li>
+	<li>really-3d-genome: survey paper about 3D genome modeling and visualization (wip)</li>
+</ul>
+
+<style>
+* {
+	font-family: "Montserrat", sans-serif;
+}
+</style>
