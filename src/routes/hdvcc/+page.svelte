d58e603cb1a7b430477297cae7d584837a4ec814
David Kouril <david.kouril@gmail.com>
2024-05-15 12:46:43 -0400

    hdvcc landing

--- src/routes/hdvcc/+page.svelte
+++ src/routes/hdvcc/+page.svelte
@@ -1,1 +1,44 @@
-<h1>hidive coding club</h1>
+<svelte:head>
+	<title>hdvcc</title>
+	<meta name="description" content="This is where the description goes for SEO" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
+	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<h1>hidive coding club.</h1>
+<ul>
+	<li>we meet every week for 1 hour.</li>
+	<li>we go through a coding book and implement the material.</li>
+	<li>we have a rotating schedule: one person from the group is typing, others are supporting.</li>
+	<li>each week’s work ends up as a pull request. all participants are tagged in the commit.</li>
+	<li>you should not do extra work outside of the club meeting.</li>
+</ul>
+
+<h2>currently.</h2>
+<p>
+	Implementing <a href="https://raytracing.github.io/books/RayTracingInOneWeekend.html">Ray Tracing in One Weekend</a> using <a href="http://ziglang.org">zig</a>.
+</p>
+<p>
+	repository: <a href="github.com">TODO</a>.
+	</p>
+
+<h2>meetings.</h2>
+<p><b>next</b>: Thursday May 15, 2024.</p>
+
+
+<style>
+* {
+	font-family: "Dosis", sans-serif;
+}
+p {
+	margin-left: 20px;
+}
+ul {
+	list-style-type: "\2328\FE0F"; 
+}
+li {
+	padding-left: 10px;
+	margin-bottom: 5px;
+}
+</style>
