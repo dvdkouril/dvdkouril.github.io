7d1c3c579e02dcc92c9a2dfe183aa8ef8daec072
David Kouril <david.kouril@gmail.com>
2024-05-21 14:01:46 -0400

    update wip page

--- src/routes/wip/+page.svelte
+++ src/routes/wip/+page.svelte
@@ -1,7 +1,7 @@
 <style>
 #body-wrapper {
 	font-family: "Comic Sans MS", "Comic Sans", Georgia;
-	font-size: 200%;
+	font-size: 150%;
 }
 
 /* unvisited link */
@@ -26,12 +26,27 @@ a:active {
 </style>
 
 <div id="body-wrapper">
-<h1>hi.</h1>
-<p><a href="https://dvdkouril.github.io/hierarchical-selections/" target="_blank">hierarchical-selections prototype</a>: hierarchical selections in a 3D chromatin model, visualization via a fixed tree-like layout.<br/>
-<a href="https://github.com/dvdkouril/hierarchical-selections" target="_blank">code</a></p>
-<p><a href="https://dvdkouril.github.io/chromatin-n-out/" target="_blank">chromatin-n-out (aka hyperwindows) prototype</a>: hierarchical selections in a 3D chromatin model, visualization via a fluid physics-based layout.<br/>
+<h1>wip.</h1>
+<p><b>chromospace</b>: 3D chromatin visualization library with grammar specification.<br/>
+<a href="https://github.com/dvdkouril/chromospace" target="_blank">code</a>
+<a href="https://observablehq.com/@david-kouril/chromospace-playground" target="_blank">observablehq</a>
+<a href="https://www.davidkouril.com/chromospace-paper/" target="_blank">paper</a></p>
+
+<p><b>chromospyce</b>: anywidget-powered version of chromospace for use in computational notebooks.<br/>
+<a href="https://github.com/dvdkouril/chromospyce" target="_blank">code</a>
+<a href="https://colab.research.google.com/drive/1E-jDzM1hs-TtTg_XJnEuDn6RU2VG0Lxe?usp=sharing" target="_blank">colab</a>
+
+<h2>backburner.</h2>
+<p><b>hyperwindows</b>: hierarchical selections in a 3D chromatin model, visualization via a fluid physics-based layout.<br/>
+<a href="https://dvdkouril.github.io/chromatin-n-out/" target="_blank">app</a>
 <a href="https://github.com/dvdkouril/chromatin-n-out" target="_blank">code</a></p>
-<p><a href="https://dvdkouril.github.io/really-3d-genome/" target="_blank">really-3d-genome</a>: survey paper about 3D genome modeling and visualization (wip)<br/>
-<a href="https://github.com/dvdkouril/really-3d-genome" target="_blank">code</a></p>
+
+<p><b>really-3d-genome</b>: survey paper about 3D genome modeling and visualization (wip)<br/>
+<a href="https://dvdkouril.github.io/really-3d-genome/" target="_blank">paper</a></p>
 <!--<p><a href="#" target="_blank">more.</a></p>-->
+
+<h2>old prototypes.</h2>
+<p><b>hierarchical-selections</b>: hierarchical selections in a 3D chromatin model, visualization via a fixed tree-like layout.<br/>
+<a href="https://dvdkouril.github.io/hierarchical-selections/" target="_blank">app</a>
+<a href="https://github.com/dvdkouril/hierarchical-selections" target="_blank">code</a></p>
 </div>
