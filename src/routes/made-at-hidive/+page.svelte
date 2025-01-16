d74f4e9f79da7abd42710d734dd2ef0db7682878
David Kouril <david.kouril@gmail.com>
2025-01-16 12:00:55 -0500

    draft

---
+++ src/routes/made-at-hidive/+page.svelte
@@ -0,0 +1,8 @@
+<h1>made at hidive</h1>
+<p>list of small things i've created while at the hidive lab.</p>
+<ul>
+	<li>HUROS 2023 poster</li>
+	<li>HUROS 2024 poster</li>
+	<li>4DN annual meeting 2024 poster (December 2024)</li>
+	<li>HIDIVE zoom background</li>
+</ul>
