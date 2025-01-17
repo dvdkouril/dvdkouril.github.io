4f17d49b0eb7157717c2c3b62ec21a4a634cb70e
David Kouril <david.kouril@gmail.com>
2025-01-17 09:10:09 -0500

    format

--- src/routes/made-at-hidive/+page.svelte
+++ src/routes/made-at-hidive/+page.svelte
@@ -1,8 +1,9 @@
 <h1>made at hidive</h1>
 <p>list of small things i've created while at the hidive lab.</p>
 <ul>
-	<li>HUROS 2023 poster</li>
-	<li>HUROS 2024 poster</li>
-	<li>4DN annual meeting 2024 poster (December 2024)</li>
-	<li>HIDIVE zoom background</li>
+	<li>[poster] HUROS 2023 poster</li>
+	<li>[poster] HUROS 2024 poster</li>
+	<li>[poster] 4DN annual meeting 2024 poster (December 2024)</li>
+	<li>[app] HIDIVE zoom background</li>
+	<li>[video] anywidget setup: developing both the library and the widget</li>
 </ul>
