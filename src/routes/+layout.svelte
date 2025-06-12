9317f3342b90825de0e1c9d1e3e3b3d1dec7aca9
David Kouřil <david.kouril@gmail.com>
2025-06-12 14:24:41 -0400

    feat: simple socials link previews (#4)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,25 @@
 	const showHeaderFor = ["/", "/cv", "/work", "/thingsimade"];
 	const pageUrl = $derived(page.url.pathname);
 	const showHeader = $derived(showHeaderFor.some((v) => pageUrl === v));
+
+	const pageTitle = "David Kouřil, a visualization researcher.";
+	const pageDescr =
+		"Personal page for David Kouřil, a postdoctoral researcher in data visualization";
 </script>
+
+<svelte:head>
+	<title>{pageTitle}</title>
+	<meta name="description" content={pageDescr} />
+	<!-- Open Graph (used by Facebook, LinkedIn, Discord, etc.) -->
+	<meta property="og:title" content={pageTitle} />
+	<meta property="og:description" content={pageDescr} />
+	<meta
+		property="og:image"
+		content="https://davidkouril.com/david-og-preview.jpg"
+	/>
+	<meta property="og:url" content="https://davidkouril.com" />
+	<meta property="og:type" content="website" />
+</svelte:head>
 
 {#if showHeader}
 	<div id="header">
